eb660a78b68829c77ee7b326850f27ca86f5ca21
Guilherme Ruiz <guiiruiz@hotmail.com>
2022-05-29 01:50:02 +1000

    Implement basic tool spawn script

--- Assets/_Scripts/ObjectSpawnManager.cs
+++ Assets/_Scripts/ObjectSpawnManager.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectSpawnManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class ObjectSpawnManager : MonoBehaviour {
+  private float SpawnDelay = 2f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+  [SerializeField] GameObject[] ToolPrefabs;
+  private float LastSpawnTime;
+
+  void Start() {
+    LastSpawnTime = Time.time;
+  }
+
+  void Update() {
+    float now = Time.time;
+    if (now - LastSpawnTime > SpawnDelay) {
+      GameObject tool = GetRandomTool();
+      SpawnTool(tool);
+
+      LastSpawnTime = now;
     }
+  }
+
+  void SpawnTool(GameObject tool) {
+    float xSpawnRange = transform.localScale.x / 2;
+    float posX = Random.Range(-xSpawnRange, xSpawnRange);
+
+    float posY = transform.transform.position.y;
+    Instantiate(tool, new Vector3(posX, posY, 0), Quaternion.identity);
+  }
+
+  GameObject GetRandomTool() {
+    return ToolPrefabs[Random.Range(0, ToolPrefabs.Length - 1)];
+  }
 }
