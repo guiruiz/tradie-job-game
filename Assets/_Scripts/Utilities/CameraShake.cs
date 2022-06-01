f7d22a646665e6caba80dfc270494f628169a4c8
Guilherme Ruiz <guiiruiz@hotmail.com>
2022-06-01 23:40:19 +1000

    Shake the camera on tool hit

---
+++ Assets/_Scripts/Utilities/CameraShake.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+  [SerializeField] int CameraShakeFrames = 100;
+  [SerializeField] float Intensity = .1f;
+  [SerializeField] float Frequency = 1f;
+
+
+  private void Awake() {
+    PlayerCollider.OnToolHit += ShakeCamera;
+  }
+
+  void ShakeCamera(Tool _) {
+    StopAllCoroutines();
+    StartCoroutine(CameraShakeCoroutine());
+  }
+
+  IEnumerator CameraShakeCoroutine() {
+    var originalposition = transform.position;
+
+    for (int i = 0; i < CameraShakeFrames; i++) {
+      transform.position = originalposition + (Vector3.right * Intensity * Mathf.Cos(i * Frequency) * (100 - i) / CameraShakeFrames);
+      yield return null;
+    }
+
+    transform.position = originalposition;
+  }
+}
+
